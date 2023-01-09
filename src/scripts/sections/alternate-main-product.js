767d9691614cbc30eeebb665ab4370f3190a3f25
EvgeniyLageev <E.Lageev@gmail.com>
2023-01-09 16:32:11 +0300

    add quantity

--- src/scripts/sections/alternate-main-product.js
+++ src/scripts/sections/alternate-main-product.js
@@ -35,6 +35,67 @@ class Accordion {
   }
 }
 
+class QuantityPicker {
+  constructor(fieldset) {
+    this.fieldset = fieldset;
+
+    this.input = this.fieldset.querySelector('.product__quantity-input');
+
+    this.buttonDecrease = this.fieldset.querySelector('.product__quantity-button--decrease');
+    this.buttonIncrease = this.fieldset.querySelector('.product__quantity-button--increase');
+  }
+
+  init = () => {
+    this.input.addEventListener('input', this.handleInput);
+    this.buttonDecrease.addEventListener('click', this.decreaseQuantity);
+    this.buttonIncrease.addEventListener('click', this.increaseQuantity);
+  }
+
+  destroy = () => {
+    this.input.removeEventListener('input', this.handleInput);
+    this.buttonDecrease.removeEventListener('click', this.decreaseQuantity);
+    this.buttonIncrease.removeEventListener('click', this.increaseQuantity);
+  }
+
+  decreaseQuantity = () => {
+    errorMessage.innerHTML = ""
+    const quantity = this.getInputValue();
+
+    if (quantity > this.input.min) {
+      this.setInputValue(quantity - 1);
+    }
+  }
+
+  increaseQuantity = () => {
+    errorMessage.innerHTML = ""
+    const quantity = this.getInputValue();
+
+    if (quantity < this.input.max) {
+      this.setInputValue(quantity + 1);
+    }
+
+  }
+
+  handleInput = () => {
+    errorMessage.innerHTML = ""
+    const quantity = this.getInputValue();
+
+    if (quantity < this.input.min) {
+      this.setInputValue(1)
+    } else if (quantity > this.input.max) {
+      this.setInputValue(this.input.max)
+    }
+  }
+
+  getInputValue = () => {
+    return Number(this.input.value);
+  }
+
+  setInputValue = (newValue) => {
+    this.input.value = newValue;
+  }
+}
+
 const form = document.querySelector(".product__form")
 const errorMessage = document.querySelector(".form__error")
 
@@ -42,6 +103,7 @@ const errorMessage = document.querySelector(".form__error")
 register("alternate-main-product", {
   accordions: {},
   productForm: null,
+  quantityPicker: null,
 
   toggle: function (event) {
     const button = event.target.querySelector('.accordion__button');
@@ -52,7 +114,6 @@ register("alternate-main-product", {
   onOptionChange: function (event) {
     errorMessage.innerHTML = ""
     const variant = event.dataset.variant;
-    console.log(variant)
 
     if (!variant) return;
 
@@ -112,21 +173,24 @@ register("alternate-main-product", {
 
 
     const productHandle = form.dataset.handle;
-    console.log(productHandle),
 
 
-      fetch(`${Shopify.routes.root}products/${productHandle}.js`)
-        .then((response) => {
-          return response.json();
-        })
-        .then((productJSON) => {
-          this.productForm = new ProductForm(form, productJSON, {
-            onOptionChange: this.onOptionChange,
-            onFormSubmit: this.onFormSubmit,
-          });
-        })
-        .catch(console.error);
+    fetch(`${Shopify.routes.root}products/${productHandle}.js`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((productJSON) => {
+        this.productForm = new ProductForm(form, productJSON, {
+          onOptionChange: this.onOptionChange,
+          onFormSubmit: this.onFormSubmit,
+        });
+      })
+      .catch(console.error);
 
+
+    const fieldsetQuantity = form.querySelector('.product__fieldset--quantity');
+    this.quantityPicker = new QuantityPicker(fieldsetQuantity);
+    this.quantityPicker.init();
   },
 
   onUnload: function () {
