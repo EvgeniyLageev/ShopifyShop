91adba3bf2185b2bd4567bb13a371b947668457a
EvgeniyLageev <E.Lageev@gmail.com>
2023-01-09 03:26:31 +0300

    add option

--- src/scripts/sections/alternate-main-product.js
+++ src/scripts/sections/alternate-main-product.js
@@ -1,4 +1,6 @@
 import { register } from '@shopify/theme-sections';
+import { getUrlWithVariant, ProductForm } from '@shopify/theme-product-form';
+import { formatMoney } from '@shopify/theme-currency';
 
 
 class Accordion {
@@ -35,50 +37,104 @@ class Accordion {
 
 const form = document.querySelector(".product__form")
 const errorMessage = document.querySelector(".form__error")
-form.addEventListener("submit", event => {
-  event.preventDefault()
-  fetch(event.target.action + ".js", {
-    method: event.target.method,
-    body: new FormData(event.target),
-    headers: {
-      "X-Requested-With": "XMLHttpRequest"
-    }
-  }).then(response => response.json())
-    .then((data) => {
-      if (data.id) {
-        const event = new CustomEvent("cart:added", {
-          detail: {
-            updatedHeader: data.sections['alternate-header'],
-          },
-          bubles: true
-        })
-        window.dispatchEvent(event);
-      } else {
-        const message = `${data.message} (${data.status}): ${data.description}`;
-        throw new Error(message);
-      }
-    })
-    .catch(error => {
-      console.log(error)
-    })
-})
-
 
 
 register("alternate-main-product", {
   accordions: {},
+  productForm: null,
+
+  toggle: function (event) {
+    const button = event.target.querySelector('.accordion__button');
+    const id = button.getAttribute('aria-controls').split('--').pop();
+    this.accordions[id].toggle();
+  },
+
+  onOptionChange: function (event) {
+    errorMessage.innerHTML = ""
+    const variant = event.dataset.variant;
+    console.log(variant)
+
+    if (!variant) return;
+
+    const url = getUrlWithVariant(window.location.href, variant.id);
+    window.history.replaceState({ path: url }, '', url);
+
+    document.querySelector(".product__code--number").innerHTML = formatMoney(
+      variant.price,
+      '{{ amount_with_comma_separator }}',
+    );
+
+    if (variant.available) {
+      document.querySelector(".button--add").disabled = false;
+    } else {
+      document.querySelector(".button--add").disabled = true;
+      errorMessage.innerHTML = "Temporarily unavailable"
+    }
+  },
+
+  onFormSubmit: function (event) {
+    event.preventDefault()
+    let formData = new FormData(event.target);
+    formData.append("sections", "alternate-header");
+
+    fetch(`${Shopify.routes.root}cart/add.js`, {
+      method: event.target.method,
+      body: formData,
+      headers: {
+        "X-Requested-With": "XMLHttpRequest"
+      }
+    }).then(response => response.json())
+      .then((data) => {
+        if (data.id) {
+          const event = new CustomEvent("cart:added", {
+            detail: {
+              updatedHeader: data.sections['alternate-header'],
+            },
+            bubles: true
+          })
+          window.dispatchEvent(event);
+        } else {
+          const message = `${data.message} (${data.status}): ${data.description}`;
+          errorMessage.innerHTML = message
+          throw new Error(message);
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  },
 
   onLoad: function () {
     const accordionButtons = document.querySelectorAll('.accordion__button');
     accordionButtons.forEach((el) => {
       this.accordions[new Accordion(el).id] = new Accordion(el);
     });
+
+
+    const productHandle = form.dataset.handle;
+    console.log(productHandle),
+
+
+      fetch(`${Shopify.routes.root}products/${productHandle}.js`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((productJSON) => {
+          this.productForm = new ProductForm(form, productJSON, {
+            onOptionChange: this.onOptionChange,
+            onFormSubmit: this.onFormSubmit,
+          });
+        })
+        .catch(console.error);
+
   },
 
   onUnload: function () {
     for (const accordion of Object.values(this.accordions)) {
       accordion.destroy();
     }
+
+    this.productForm.destroy();
   },
 
   onSelect: function () {
@@ -88,15 +144,11 @@ register("alternate-main-product", {
   },
 
   onBlockSelect(evt) {
-    const button = evt.target.querySelector('.accordion__button');
-    const id = button.getAttribute('aria-controls').split('--').pop();
-    this.accordions[id].toggle();
+    this.toggle(evt)
   },
 
   onBlockDeselect(evt) {
-    const button = evt.target.querySelector('.accordion__button');
-    const id = button.getAttribute('aria-controls').split('--').pop();
-    this.accordions[id].toggle();
+    this.toggle(evt)
   },
 });
 
